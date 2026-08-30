fceed2c6fad97c65e733ef7510a95687220b40cf
beastmahesh <24uca243@anjaconline.org>
2026-08-30 18:56:43 +0530

    Fix database connection timeouts locally, clean up audit previews

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8']); // Override DNS servers to resolve Atlas querySrv ECONNREFUSED errors on Windows
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
